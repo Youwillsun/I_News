cb58277c1d6a5fc18d0b8cbc977847bbe1c7b897
sunyongwei <2580265674@qq.com>
2020-03-10 13:16:01 +0800

    增加假数据，限制空评论发表。

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-comment',
@@ -13,21 +14,37 @@ export class CommentComponent implements OnInit {
   // 存储用户写的评论内容
   public comContent: string;
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() { }
 
   // 发表评论
   public publishCom() {
-    // 获取昵称
-    let nickName = window.localStorage.getItem('nickName');
-    console.log(nickName);
-    this.comment.unshift({ userId: window.localStorage.getItem('userId'), nickName: nickName === null || undefined ? '暂无' : nickName, headPhoto: "https://s2.ax1x.com/2020/03/03/34B4cF.png", content: this.comContent, like: 0 });
+    if (this.comContent) {
+      // 获取昵称
+      let nickName = window.localStorage.getItem('nickName');
+      this.comment.unshift({ userId: window.localStorage.getItem('userId'), nickName: nickName === null || undefined ? '暂无' : nickName, headPhoto: "https://s2.ax1x.com/2020/03/03/34B4cF.png", content: this.comContent, like: 0 });
+    } else { 
+      this.presentToast();
+      return false;
+    }
+
   }
 
   // 点赞
   likeEvent(num: number) {
     console.log(num);
+  }
+
+  // ionic toast
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: '不能发表空评论！',
+      duration: 1000,
+      position:'top',
+      color:'danger'
+    });
+    toast.present();
   }
 
 }
